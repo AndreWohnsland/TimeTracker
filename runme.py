027f71b6fbbb88afb26d0587f288802f56cce52f
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-04-15 19:23:05 +0200

    Add dark theme with auto detect

--- runme.py
+++ runme.py
@@ -1,11 +1,27 @@
 import sys
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import pyqtSlot
+
+import qdarktheme
+import darkdetect
 
 from src.ui_mainwindow import MainWindow
+
+IS_LIGHT: bool = darkdetect.isLight()  # type: ignore
+STYLE_NAME = "light" if IS_LIGHT else "dark"
+
+
+@pyqtSlot()
+def sync_theme() -> None:
+    stylesheet = qdarktheme.load_stylesheet(STYLE_NAME)
+    QApplication.instance().setStyleSheet(stylesheet)  # type: ignore
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     w = MainWindow()
+    # in case of active style change, also change theme, need to sync at start
+    app.paletteChanged.connect(sync_theme)
+    sync_theme()
     w.show()
     sys.exit(app.exec_())
