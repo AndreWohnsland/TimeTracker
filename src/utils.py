a85bf14f9b2dbf365e167f65d7829fe55d94826d
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-04-18 20:35:11 +0200

    Do not use icon states in utils

--- src/utils.py
+++ src/utils.py
@@ -1,27 +1,23 @@
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtGui import QIcon
 
 import qdarktheme
 import darkdetect
 
-from src.filepath import CLOCK_ICON, CLOCK_ICON_DARK_MODE
+
+def is_light() -> bool:
+    """Returns if the system uses dark or light mode"""
+    return darkdetect.isLight()  # type: ignore
 
 
 def get_style_name() -> str:
     """Returns if the system uses dark or light mode"""
-    is_light: bool = darkdetect.isLight()  # type: ignore
-    return "light" if is_light else "dark"
+    return "light" if is_light() else "dark"
 
 
 def get_icon_color() -> str:
     """Returns the icon color based on the light mode."""
-    return "black" if darkdetect.isLight() else "white"
-
-
-def get_app_icon() -> QIcon:
-    """Returns the icon color based on the light mode."""
-    return QIcon(str(CLOCK_ICON)) if darkdetect.isLight() else QIcon(str(CLOCK_ICON_DARK_MODE))
+    return "black" if is_light() else "white"
 
 
 @pyqtSlot()
