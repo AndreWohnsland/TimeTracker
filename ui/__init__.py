63ea0b2b595e9d585ce2a4c4776831bee353975a
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-04-29 20:00:17 +0200

    Add ui elements

---
+++ ui/__init__.py
@@ -0,0 +1,9 @@
+from ui.data_widget import Ui_DataWidget
+from ui.config_window import Ui_ConfigWindow
+from ui.mainwindow import Ui_MainWindow
+
+__all__ = [
+    "Ui_MainWindow",
+    "Ui_DataWidget",
+    "Ui_ConfigWindow",
+]
