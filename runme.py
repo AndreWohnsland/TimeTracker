b9936739068203eaf914cbd803f1d2e74c46388f
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2025-07-07 18:00:50 +0200

    add info for import + execute order

--- runme.py
+++ runme.py
@@ -1,5 +1,6 @@
 from src.utils import prepare_data_location_and_files, setup_logging
 
+# Prepare the data location and files. meeds to be done before importing the main application
 setup_logging()
 prepare_data_location_and_files()
 
